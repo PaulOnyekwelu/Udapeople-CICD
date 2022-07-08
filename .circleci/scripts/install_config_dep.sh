c139828516233a0ffbb4ad9909bf6091288fa0e5
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-07-08 18:14:42 +0100

    resolving path bug

--- .circleci/scripts/install_config_dep.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apk update -y
-apk upgrade -y
-apk add --update tar gzip
-apk add --update ansible
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
