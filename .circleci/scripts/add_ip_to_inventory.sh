149f28a7c24be855a442a67a35ce80b3f1c6eca4
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-07-08 20:01:40 +0100

    running all jobs - final test

--- .circleci/scripts/add_ip_to_inventory.sh
+++ .circleci/scripts/add_ip_to_inventory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+ls -al 
 aws ec2 describe-instances \
   --query 'Reservations[].Instances[].PublicIpAddress' \
   --filters "Name=tag:Project,Values=Ansible" \
-  --output text >> .circleci/ansible/inventory.txt
+  --output text >> /root/project/.circleci/ansible/inventory.txt
