065ee5b5b2efd0d6f0a452ca934101d013a4a2b3
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-07-08 20:38:35 +0100

    finally - path issue resolved

--- .circleci/scripts/add_ip_to_inventory.sh
+++ .circleci/scripts/add_ip_to_inventory.sh
@@ -2,5 +2,7 @@
 ls -al 
 aws ec2 describe-instances \
   --query 'Reservations[].Instances[].PublicIpAddress' \
-  --filters "Name=tag:Project,Values=Ansible" \
-  --output text >> /root/project/.circleci/ansible/inventory.txt
+  --filters "Name=tag:project,Values=udapeople" \
+  --output text >> .circleci/ansible/inventory.txt
+
+cat .circleci/ansible/inventory.txt
