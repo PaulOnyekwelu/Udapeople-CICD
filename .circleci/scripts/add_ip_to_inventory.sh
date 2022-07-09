e12250a6e7c1c3b582166bc3c7738e5b6637b538
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-07-09 17:32:25 +0100

    revamping of deploy confi guration

--- .circleci/scripts/add_ip_to_inventory.sh
+++ .circleci/scripts/add_ip_to_inventory.sh
@@ -2,5 +2,5 @@
  
 aws ec2 describe-instances \
   --query 'Reservations[].Instances[].PublicIpAddress' \
-  --filters "Name=tag:project,Values=udapeople" \
+  --filters "Name=tag:project,Values=udapeople-$1" \
   --output text >> .circleci/ansible/inventory.txt
--- .circleci/scripts/backend_deploy.sh
+++ .circleci/scripts/backend_deploy.sh
@@ -4,4 +4,4 @@ aws cloudformation deploy \
     --template-file .circleci/files/backend.yml \
     --stack-name "udapeople-backend-$1" \
     --parameter-overrides ID="$1"  \
-    --tags project=udapeople
+    --tags project="udapeople-$1"
