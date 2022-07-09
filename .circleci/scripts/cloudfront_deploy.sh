83af7a494c0ea1db7a1fb0e8cface8cc6ed9527a
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-07-09 19:29:08 +0100

    cloudfront-update job

--- .circleci/scripts/cloudfront_deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-aws cloudformation deploy \
-         --template-file cloudfront.yml \
-         --stack-name InitialStack\
-         --parameter-overrides WorkflowID=udapeople-silcorpp
