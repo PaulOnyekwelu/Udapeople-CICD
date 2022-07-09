6c4279aa5c5cc4a6b67986cbb2a0d1da360269c4
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2022-07-09 09:05:49 +0100

    code refactoring - 2

--- .circleci/scripts/add_ip_to_inventory.sh
+++ .circleci/scripts/add_ip_to_inventory.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-ls -al 
+ 
 aws ec2 describe-instances \
   --query 'Reservations[].Instances[].PublicIpAddress' \
   --filters "Name=tag:project,Values=udapeople" \
   --output text >> .circleci/ansible/inventory.txt
-
-cat .circleci/ansible/inventory.txt
